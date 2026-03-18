927e7fd6afb22516d2144523d66c269612bb13a9
Cristian Gallardo <info@vorianglobal.com>
2026-03-18 23:02:51 +0000

    se queda cargando en Importar 214 registros, lleva asi 2 minutos

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,9 +1,10 @@
+'use client';
+
 import {initializeApp, getApps, type FirebaseApp} from 'firebase/app';
-import {getAuth, type Auth, connectAuthEmulator} from 'firebase/auth';
+import {getAuth, type Auth} from 'firebase/auth';
 import {
   getFirestore,
   type Firestore,
-  connectFirestoreEmulator,
 } from 'firebase/firestore';
 import {firebaseConfig} from './config';
 
@@ -31,29 +32,12 @@ let firestore: Firestore;
 export function initializeFirebase() {
   if (getApps().length === 0) {
     app = initializeApp(firebaseConfig);
-    auth = getAuth(app);
-    firestore = getFirestore(app);
-
-    // After initializing, you can connect to emulators if you're in a dev environment
-    if (process.env.NEXT_PUBLIC_USE_EMULATORS === 'true') {
-      const host = process.env.NEXT_PUBLIC_EMULATOR_HOST || 'localhost';
-      // It's important to check if the emulators are already running to avoid errors
-      // @ts-ignore - _isInitialized is not in the type definition
-      if (!auth.emulatorConfig) {
-        connectAuthEmulator(auth, `http://${host}:9099`, {
-          disableWarnings: true,
-        });
-      }
-      // @ts-ignore - _isInitialized is not in the type definition
-      if (!firestore._isInitialized) {
-        connectFirestoreEmulator(firestore, host, 8080);
-      }
-    }
   } else {
     app = getApps()[0];
-    auth = getAuth(app);
-    firestore = getFirestore(app);
   }
+  
+  auth = getAuth(app);
+  firestore = getFirestore(app);
 
   return {app, auth, firestore};
 }
