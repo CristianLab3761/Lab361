673b72116ae6856df88c627593947e3789c50b89
Cristian Gallardo <info@vorianglobal.com>
2026-03-18 23:10:11 +0000

    Try fixing this error: `Console Error: Firestore Permission Error: "Miss

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,7 +7,7 @@ export const firebaseConfig = {
   "apiKey": "AIzaSyD_qHaYdVYh4VrE2ryDmlJSqL61ha7A1D8",
   "authDomain": "studio-3067503220-b7d50.firebaseapp.com",
   "projectId": "studio-3067503220-b7d50",
-  "storageBucket": "studio-3067503220-b7d50.appspot.com",
+  "storageBucket": "studio-3067503220-b7d50.firebasestorage.app",
   "messagingSenderId": "959904790225",
   "appId": "1:959904790225:web:2bd6934ed297c37eacf7f0"
 };
